c83e834147071b5623bed236329c6e28c25fac7f
adamajay <30343197+adamajay@users.noreply.github.com>
2017-07-24 09:31:54 +0530

    Update count.c

--- count.c
+++ count.c
@@ -9,9 +9,9 @@ int main()
 
     while(n != 0)
     {
-        // n = n/10
-        n /= 10;
-        ++count;
+  // n = n/10
+  n /= 10;
+  ++count;
     }
 
     printf("Number of digits: %d", count);
